43e13d612e23922c6a289f9d79b8f189bfeff55c
Sarah Li <sarcl@hotmail.com>
2018-06-20 17:40:59 +0000

    Done.

--- currency.rb
+++ currency.rb
@@ -1,1 +1,14 @@
 # Write your cod here
+def usd_to_eur("dollars")
+  dollars = 1.2
+  puts dollars * 0.83
+end
+
+
+def eur_to_usd
+  euros = 1
+  puts money * 1.2
+end
+def 
+usd_to_eur
+eur_to_usd
