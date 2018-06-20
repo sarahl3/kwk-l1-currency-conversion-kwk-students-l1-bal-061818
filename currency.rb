5df47529e4f3d00754d5ee927288f534ae12c8fa
Sarah Li <sarcl@hotmail.com>
2018-06-20 17:22:07 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -1,10 +1,10 @@
 # Write your cod here
 money = 10
 def usd_to_eur
-  money = money * 0.83
+  puts money = money * 0.83
 end
 
 
 def eur_to_usd
-  money = money * 1.2
+  puts money = money * 1.2
 end
